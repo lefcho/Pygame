f14876c5099158dd1ea9909ddd9cf92bbe497193
Ivan Kostov <ivan.d.kostov@gmail.com>
2023-08-20 23:37:03 +0300

    day 1

---
+++ First_game/first_game.py
@@ -0,0 +1,41 @@
+import os
+import pygame
+
+WIDTH, HEIGHT = 900, 500
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("First Game")
+
+WHITE = (255, 255, 255)
+
+FPS = 60
+
+YELLOW_SPACESHIP_IMG = pygame.image.load(
+    os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMG, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+RED_SPACESHIP_IMG = pygame.image.load(
+    os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP_IMG, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+
+
+def draw_window():
+    WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP, (300, 100))
+    pygame.display.update()
+
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        draw_window()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
