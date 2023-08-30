2a39e4471aada4edc8ca83984c07d013441698fc
ivand <ivan.d.kostov@gmail.com>
2023-08-30 20:58:57 +0300

    class button

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,15 +1,24 @@
 import pygame
-import button
 import os
+
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1000
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Button Demo')
+
+start_img_unselected = pygame.image.load(os.path.join('Pictures', 'Start_game-selected.png'))
+start_img_selected = pygame.image.load(os.path.join('Pictures', 'Start_game - selected.png'))
+exit_img_unselected = pygame.image.load(os.path.join('Pictures', 'Exit-unselected.png'))
+exit_img_selected = pygame.image.load(os.path.join('Pictures', 'Exit - selected.png'))
 
 
 class Button:
-    def __init__(self, x, y, image_unselected, image_selected, scale):
-        width = image.get_width()
-        height = image.get_height()
-        self.image = pygame.transform.scale(image_unselected, (int(width * scale), int(height * scale)))
+    def __init__(self, x, y, image_unselected, image_selected,):
+        self.image = image_unselected
         self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
+        self.x_pos = x
+        self.y_pos = y
         self.clicked = False
 
     def draw(self, surface):
@@ -21,44 +30,36 @@ class Button:
                 self.clicked = True
                 action = True
 
-        if pygame.mouse.get_pressed()[0] == 0:
+        if not pygame.mouse.get_pressed()[0]:
             self.clicked = False
 
-        surface.blit(self.image, (self.rect.x, self.rect.y))
-
+        surface.blit(self.image, (self.x_pos, self.y_pos))
         return action
 
 
-SCREEN_HEIGHT = 500
-SCREEN_WIDTH = 800
+start_button = Button(100, 200, start_img_unselected, start_img_selected)
+exit_button = Button(0, 0, exit_img_unselected, exit_img_selected)
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Button Demo')
 
-start_img_unselected = pygame.image.load(os.path.join('Pictures', 'Start_game - unselected.png'))
-start_img_selected = pygame.image.load(os.path.join('Pictures', 'Start_game - selected.png'))
-exit_img_unselected = pygame.image.load(os.path.join('Pictures', 'Exit - unselected.png'))
-exit_img_selected = pygame.image.load(os.path.join('Pictures', 'Exit - selected.png'))
+def main():
+    run = True
+    while run:
 
-start_button = Button(100, 200, start_img_unselected, start_img_selected, 0.8)
-exit_button = Button(450, 200, exit_img_unselected, exit_img_selected, 0.8)
+        screen.fill((202, 228, 241))
 
-run = True
-while run:
+        if start_button.draw(screen):
+            print('START')
+        if exit_button.draw(screen):
+            print('exiT')
 
-    screen.fill((202, 228, 241))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-    if start_button.draw(screen):
-        print('START')
-    if exit_button.draw(screen):
-        print('EXIT')
+        pygame.display.update()
 
-    # event handler
-    for event in pygame.event.get():
-        # quit game
-        if event.type == pygame.QUIT:
-            run = False
+    pygame.quit()
 
-    pygame.display.update()
 
-pygame.quit()
+if __name__ == "__main__":
+    main()
