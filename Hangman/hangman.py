372537c62ae5d296b71b7f5d112be6c6155385c9
Ivan Kostov <ivan.d.kostov@gmail.com>
2023-09-08 21:30:46 +0300

    background

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -12,14 +12,14 @@ START_BUTTON_H = 169
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Hangman')
-
+TITLE_BACKGROUND = pygame.image.load(os.path.join('Pictures', 'background_with_title.png'))
 start_img_unselected = pygame.image.load(os.path.join('Pictures', 'Start_game - unselected.png'))
 start_img_selected = pygame.image.load(os.path.join('Pictures', 'Start_game - selected.png'))
 exit_img_unselected = pygame.image.load(os.path.join('Pictures', 'Exit - unselected.png'))
 exit_img_selected = pygame.image.load(os.path.join('Pictures', 'Exit - selected.png'))
 
 start_button = (button.Button
-                (SCREEN_WIDTH / 2 - START_BUTTON_W / 2, SCREEN_HEIGHT / 2 - START_BUTTON_H / 2,
+                (SCREEN_WIDTH / 2 - START_BUTTON_W / 2, SCREEN_HEIGHT / 2 - START_BUTTON_H / 2 - 90,
                  start_img_unselected, start_img_selected))
 exit_button = button.Button(SCREEN_WIDTH - EXIT_BUTTON_W - 10, 10, exit_img_unselected, exit_img_selected)
 
@@ -27,14 +27,19 @@ exit_button = button.Button(SCREEN_WIDTH - EXIT_BUTTON_W - 10, 10, exit_img_unse
 def main():
     clock = pygame.time.Clock()
     run = True
+    game_started = False
     while run:
         clock.tick(FPS)
-        screen.fill((202, 228, 241))
+
+        if not game_started:
+            screen.blit(TITLE_BACKGROUND, (0, 0))
+
+            if start_button.draw(screen):
+                game_started = True
+                print('START')
 
         if exit_button.draw(screen):
             run = False
-        if start_button.draw(screen):
-            print('START')
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
